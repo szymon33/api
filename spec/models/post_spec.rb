064e329fe2e139a3a94e67157270c4ec3de6ad59
szymonm <miedziejko@gmail.com>
2016-01-18 18:50:04 +0100

    Refactoring Post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Post do
+  let(:post) { FactoryGirl.create(:post) }
+
   it { validate_presence_of :content }
   it { validate_presence_of :title }
   it { validate_presence_of :creator }
@@ -8,19 +10,16 @@ describe Post do
   it { have_many :comments }
 
   describe 'when create' do
-    before(:each) { @post = FactoryGirl.create(:post) }
-
     it 'is valid' do
-      expect(@post).to be_valid
+      expect(post).to be_valid
     end
 
     it 'has like counter eql 0' do
-      expect(@post.like_counter).to be_zero
+      expect(post.like_counter).to be_zero
     end
   end
 
   it 'can increase like counter' do
-    post = FactoryGirl.create(:post)
     expect {
       post.like
     }.to change { post.like_counter }.by(1)
