8e4b4b995d9ed07d35cffea5481c71f1899e6c03
szymonm <miedziejko@gmail.com>
2016-01-19 16:49:45 +0100

    Add Seeds file with sample data for each model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ActiveRecord::Base
   include Like
 
-  attr_accessible :content, :title, :like_counter
+  attr_accessible :content, :title, :like_counter, :user_id
 
   has_many :comments, dependent: :destroy
+  accepts_nested_attributes_for  :comments
+
   belongs_to :creator, class_name: 'User', foreign_key: :user_id
   validates_presence_of :content, :title, :creator
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+guest = User.create(username: 'pokemon', password: '123')
+user = User.create(username: 'gates', password: '123', role: 'user')
+admin = User.create(username: 'jobs', password: '123', role: 'admin')
+
+user_post = Post.create!(
+  title: 'Stop Using These 16 Terms to Describe Yourself',
+  content: "Do you describe yourself differently -- on your website, promotional materials, or especially on social media -- than you do in person? Do you use cheesy clichés and overblown superlatives and breathless adjectives? Do you write things about yourself you would never have the nerve to actually say?",
+  user_id: user.id
+)
+
+admin_post = Post.create!(
+  title: "12 Reasons Why I Won’t Add You To My LinkedIn Connections",
+  content: 'Please allow me to disagree with what your subconscious might be telling you after reading the title of my article. I am neither arrogant nor egotistical.',
+  user_id: admin.id
+)
+
+user_post.comments.build(
+  content: "Content is this and that and don't forget to say it's great!",
+  user_id: admin.id
+)
+user_post.save!
+
+admin_post.comments.build(
+  content: "Europe's fastest growing online auction house, featuring a wide range of special objects. Have a look at our auctions and place your bids!",
+  user_id: user.id
+)
+admin_post.save!
+
+user_post.like
