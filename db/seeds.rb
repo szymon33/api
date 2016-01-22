0ee127a08463edc738a991781002f733f3b93970
szymonm <miedziejko@gmail.com>
2016-01-22 19:10:40 +0100

    Refactoring seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.new(username: 'gates', role: 'user')
+user = User.new(username: 'user', role: 'user')
 user.password = '123'
 user.save!
 
-admin = User.new(username: 'jobs', role: 'admin')
+admin = User.new(username: 'admin', role: 'admin')
 admin.password = '123'
 admin.save!
 
-user_post = Post.new(
+user_post = user.posts.build(
   title: 'Stop Using These 16 Terms to Describe Yourself',
   content: "Do you describe yourself differently -- on your website, promotional materials, or especially on social media -- than you do in person? Do you use cheesy clichés and overblown superlatives and breathless adjectives? Do you write things about yourself you would never have the nerve to actually say?"
 )
 user_post.creator = user
-user.save!
+user_post.save!
 
-admin_post = Post.new(
+admin_post = admin.posts.build(
   title: "12 Reasons Why I Won’t Add You To My LinkedIn Connections",
   content: 'Please allow me to disagree with what your subconscious might be telling you after reading the title of my article. I am neither arrogant nor egotistical.'
 )
-admin_post.creator = admin
-admin.save!
+admin_post.save!
 
 admin_comment = admin.comments.build(
   content: "Content is this and that and don't forget to say it's great!"
